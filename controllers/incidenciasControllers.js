3fafde8664489f4f0a88dea4d2e1b23b98bf0f4f
kennerry <00308726@uca.edu.sv>
2026-09-14 23:23:49 -0600

    feat(incidencias): implementar endpoint DELETE /incidencias/:id con findIndex y splice

--- controllers/incidenciasControllers.js
+++ controllers/incidenciasControllers.js
@@ -26,7 +26,25 @@ const cambiarEstado = (req, res) => {
     return res.json({mensaje: "Estado actualizado con éxito", incidencia});
 };
 
+//DELETE /incidencias/:id
+const eliminarIncidencia = (req, res) => {
+    const id = parseInt(req.params.id, 10)
+
+    //Busca la posicion en el arreglo
+    const indice = incidencias.findIndex((item) => item.id === id);
+    
+    //Valida que exista
+    if (indice === -1 ) {
+        return res.status(404).json({mensaje: "Incidencia encontrada"});
+    }
+    //Elimina exactamente un elemento
+    incidencias.splice(indice, 1);
+
+    return res.json({mensaje: "Incidencia eliminada correctamente"});
+}
+
 module.exports = {
     incidencias,
     cambiarEstado,
+    eliminarIncidencia
 }; 
--- routes/incidencias.js
+++ routes/incidencias.js
@@ -4,4 +4,5 @@ const controller = require("../controllers/incidenciasControllers");
 
 // Como app.js ya monta "/incidencias", aquí la ruta es relativa: /:id/estado
 router.put("/:id/estado", controller.cambiarEstado);
+router.delete("/:id", controller.eliminarIncidencia);
 module.exports = router;
